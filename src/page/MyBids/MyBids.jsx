07a262418abd609a3d873d886f81fe73d4704556
shariyer132 <shariyer132@gmail.com>
2023-11-07 20:50:36 +0600

    bid jobs load successfully

--- src/page/MyBids/MyBids.jsx
+++ src/page/MyBids/MyBids.jsx
@@ -1,6 +1,16 @@
+import axios from "axios";
+import { useEffect } from "react";
+// import { useEffect, useState } from "react";
 
 const MyBids = () => {
+    // const [bidJobs, setBidJobs] = useState([]);
 
+    useEffect(()=>{
+        axios('http://localhost:5000/bidJobs')
+        .then(res=>{
+            console.log(res.data);
+        })
+    },[])
     return (
         <div>
             <h2>my bids</h2>
