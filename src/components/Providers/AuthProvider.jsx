0dbddc80d86f2a3147df3d419500522702b55e01
shariyer132 <shariyer132@gmail.com>
2023-11-08 16:59:37 +0600

    token set and token removed when user log out

--- src/components/Providers/AuthProvider.jsx
+++ src/components/Providers/AuthProvider.jsx
@@ -37,11 +37,18 @@ const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            const userEmail = currentUser?.email || user?.email
+            const loggedUser = { email: userEmail }
             setUser(currentUser);
             setLoading(false);
             if (currentUser) {
-                const loggedUser = { email: currentUser.email }
-                axios.post(loggedUser, { withCredentials: true })
+                axios.post('http://localhost:5000/jwt', loggedUser, { withCredentials: true })
+                    .then(res => {
+                        console.log('token response', res.data);
+                    })
+            }
+            else {
+                axios.post('http://localhost:5000/logout', loggedUser, { withCredentials: true })
                     .then(res => {
                         console.log(res.data);
                     })
